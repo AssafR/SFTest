7bb2b64288d060d747f576d94538bddb83aa6500
AssafR <assaf@razon-family.com>
2018-12-31 10:03:22 +0200

    Added Pickle

--- classify_acquisition.py
+++ classify_acquisition.py
@@ -1,11 +1,14 @@
 import warnings;
+import pickle
+import os.path
 
 warnings.filterwarnings("ignore")
 import pandas as pd
+import pre_processor
 
 
 def print_statistics(frame):
-    print("Columns:",frame.columns)
+    print("Columns:", frame.columns)
     print("Head:")
     print(frame.head())
     print("Description:")
@@ -18,18 +21,28 @@ def print_statistics(frame):
 def read_data(filename):
     frame = pd.read_csv(filename)
     frame = frame.dropna(axis=0, subset=['class'])
-    frame = frame.astype({'class': int})
-    frame.rename(index=str, columns={'Unnamed: 0': 'number', 'class': 'result'},inplace=True)
-    print("Columns:",frame.columns)
-    frame.set_index('number',inplace=True)
+    frame = frame.astype({'class': int, 'title':str})
+    frame.rename(index=str, columns={'Unnamed: 0': 'number', 'class': 'result'}, inplace=True)
+    print("Columns:", frame.columns)
+    frame.set_index('number', inplace=True)
     return frame
 
+
+def contains(substring: str, string: str):
+    return int(substring in string)
 
 
 # General initialization
 pd.set_option('display.width', 1000)
 pd.set_option('display.expand_frame_repr', False)
 csv_filename = "gold_data.csv"
+pickle_filename = "data.pkl"
 
-df = read_data(csv_filename)
+if not os.path.isfile(pickle_filename):
+    df = read_data(csv_filename)
+    pickle.dump(df, open(pickle_filename, "wb"))
+else:
+    df = pickle.load(open(pickle_filename,"rb"))
+
+df['title_contains_acquisition'] = df["title"].apply(lambda x: contains("acquisition", x))
 print_statistics(df)
